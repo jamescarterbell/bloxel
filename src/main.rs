284087636b0e50ef1a1501e1905b5c50573eed75
jamescarterbell <jamescarterbell@gmail.com>
2019-08-10 23:15:42 -0400

    First Commit

---
+++ src/main.rs
@@ -0,0 +1,112 @@
+mod renderer;
+
+use renderer::*;
+use winit::dpi::*;
+use winit::*;
+
+#[allow(unused_imports)]
+use log::{debug, error, info, trace, warn};
+
+fn main() {
+    simple_logger::init().unwrap();
+
+    let mut winit_state = WinitState::default();
+    let mut hal_state = HalState::new(&winit_state.window, "New Window").unwrap();
+
+    loop {
+        let inputs = UserInput::poll_events_loop(&mut winit_state.events_loop);
+        if inputs.end_requested {
+            break;
+        }
+        if let Some((x, y)) = inputs.new_frame_size {}
+        if let Err(e) = render(&mut hal_state) {
+            error!("Rendering Error: {:?}", e);
+            break;
+        }
+    }
+}
+
+pub fn render(hal: &mut HalState) -> Result<(), &str> {
+    match hal.draw_clear_frame([0.1, 0.5, 0.75, 1.0]) {
+        Ok(_val) => Ok(()),
+        Err(e) => panic!(e),
+    }
+}
+
+#[derive(Debug)]
+pub struct WinitState {
+    pub events_loop: EventsLoop,
+    pub window: Window,
+}
+
+impl WinitState {
+    /// Constructs a new 'EvetnsLoop' and 'Window" pair.
+    /// Use the specified title and size (if not full screen)
+    pub fn new<T: Into<String>>(
+        title: T,
+        size: LogicalSize,
+        full_screen: bool,
+    ) -> Result<Self, CreationError> {
+        let events_loop = EventsLoop::new();
+        let output = WindowBuilder::new()
+            .with_title(title)
+            .with_maximized(full_screen)
+            .with_dimensions(size)
+            .with_resizable(true)
+            .with_min_dimensions(LogicalSize {
+                width: 400.0,
+                height: 300.0,
+            })
+            .build(&events_loop);
+        output.map(|window| Self {
+            events_loop,
+            window,
+        })
+    }
+}
+
+impl Default for WinitState {
+    fn default() -> Self {
+        Self::new(
+            "New Window",
+            LogicalSize {
+                width: 400.0,
+                height: 300.0,
+            },
+            false,
+        )
+        .expect("Could not create a window!")
+    }
+}
+
+#[derive(Debug, Clone, Default)]
+pub struct UserInput {
+    pub end_requested: bool,
+    pub new_frame_size: Option<(f64, f64)>,
+    pub new_mouse_position: Option<(f64, f64)>,
+}
+impl UserInput {
+    pub fn poll_events_loop(events_loop: &mut EventsLoop) -> Self {
+        let mut output = UserInput::default();
+        events_loop.poll_events(|event| match event {
+            Event::WindowEvent {
+                event: WindowEvent::CloseRequested,
+                ..
+            } => output.end_requested = true,
+            Event::WindowEvent {
+                event: WindowEvent::Resized(logical),
+                ..
+            } => {
+                output.new_frame_size = Some((logical.width, logical.height));
+            }
+            Event::WindowEvent {
+                event: WindowEvent::CursorMoved { position, .. },
+                ..
+            } => {
+                output.new_mouse_position = Some((position.x, position.y));
+            }
+            _ => (),
+        });
+        output
+    }
+}
---
+++ src/renderer.rs
@@ -0,0 +1,498 @@
+#![allow(clippy::len_zero)]
+
+#[allow(unused_imports)]
+use gfx_hal::window::Extent2D;
+use gfx_hal::window::Suboptimal;
+use log::{debug, error, info, trace, warn};
+use winit::Window;
+
+#[cfg(feature = "dx12")]
+use gfx_backend_dx12 as back;
+#[cfg(feature = "metal")]
+use gfx_backend_metal as back;
+#[cfg(feature = "vulkan")]
+use gfx_backend_vulkan as back;
+
+use arrayvec::ArrayVec;
+
+#[allow(unused_imports)]
+use core::mem::ManuallyDrop;
+
+#[allow(unused_imports)]
+use gfx_hal::{
+    adapter::{Adapter, PhysicalDevice},
+    command::{ClearColor, ClearValue, CommandBuffer, MultiShot, Primary},
+    device::Device,
+    format::{Aspects, ChannelType, Format, Swizzle},
+    image::{Extent, Layout, SubresourceRange, Usage, ViewKind},
+    pass::{Attachment, AttachmentLoadOp, AttachmentOps, AttachmentStoreOp, SubpassDesc},
+    pool::{CommandPool, CommandPoolCreateFlags},
+    pso::{PipelineStage, Rect},
+    queue::{family::QueueGroup, Submission},
+    window::{PresentMode, Swapchain, SwapchainConfig},
+    Backend, Features, Gpu, Graphics, Instance, QueueFamily, Surface,
+};
+
+pub struct HalState {
+    current_frame: usize,
+    frames_in_flight: usize,
+    in_flight_fences: Vec<<back::Backend as Backend>::Fence>,
+    render_finished_semaphores: Vec<<back::Backend as Backend>::Semaphore>,
+    image_available_semaphores: Vec<<back::Backend as Backend>::Semaphore>,
+    command_buffers: Vec<CommandBuffer<back::Backend, Graphics, MultiShot, Primary>>,
+    command_pool: ManuallyDrop<CommandPool<back::Backend, Graphics>>,
+    framebuffers: Vec<<back::Backend as Backend>::Framebuffer>,
+    image_views: Vec<(<back::Backend as Backend>::ImageView)>,
+    render_pass: ManuallyDrop<<back::Backend as Backend>::RenderPass>,
+    render_area: Rect,
+    queue_group: QueueGroup<back::Backend, Graphics>,
+    swapchain: ManuallyDrop<<back::Backend as Backend>::Swapchain>,
+    device: ManuallyDrop<back::Device>,
+    _adapter: Adapter<back::Backend>,
+    _surface: <back::Backend as Backend>::Surface,
+    _instance: ManuallyDrop<back::Instance>,
+}
+
+impl HalState {
+    pub fn new(window: &Window, name: &str) -> Result<Self, &'static str> {
+        let instance = back::Instance::create(name, 1);
+        let mut surface = instance.create_surface(window);
+
+        let adapter = instance
+            .enumerate_adapters()
+            .into_iter()
+            .find(|a| {
+                a.queue_families
+                    .iter()
+                    .any(|qf| qf.supports_graphics() && surface.supports_queue_family(qf))
+            })
+            .ok_or("Couldn't find a grpahical Adapter!")?;
+
+        let (device, queue_group) = {
+            let queue_family = adapter
+                .queue_families
+                .iter()
+                .find(|qf| qf.supports_graphics() && surface.supports_queue_family(qf))
+                .ok_or("Couldn't find a QueueFamily with graphics!")?;
+            let Gpu { device, mut queues } = unsafe {
+                adapter
+                    .physical_device
+                    .open(&[(&queue_family, &[1.0; 1])], Features::empty())
+                    .map_err(|_| "Couldn't open the PhysicalDevice!")?
+            };
+            let queue_group = queues
+                .take::<Graphics>(queue_family.id())
+                .ok_or("Couldn't take ownership of the QueueGroup!")?;
+            let _ = if queue_group.queues.len() > 0 {
+                Ok(())
+            } else {
+                Err("The QueueGroup did not have any CommandQueues available!")
+            }?;
+            (device, queue_group)
+        };
+
+        // Create swapchain stuff
+        let (swapchain, extent, backbuffer, format, frames_in_flight) =
+            Self::create_swapchain(&mut surface, &adapter, &device, None);
+
+        let (image_available_semaphores, render_finished_semaphores, in_flight_fences) = {
+            let mut image_available_semaphores: Vec<<back::Backend as Backend>::Semaphore> = vec![];
+            let mut render_finished_semaphores: Vec<<back::Backend as Backend>::Semaphore> = vec![];
+            let mut in_flight_fences: Vec<<back::Backend as Backend>::Fence> = vec![];
+            for _ in 0..frames_in_flight {
+                in_flight_fences.push(
+                    device
+                        .create_fence(true)
+                        .map_err(|_| "Could not create a fence!")?,
+                );
+                image_available_semaphores.push(
+                    device
+                        .create_semaphore()
+                        .map_err(|_| "Could not create a semaphore!")?,
+                );
+                render_finished_semaphores.push(
+                    device
+                        .create_semaphore()
+                        .map_err(|_| "Could not create a semaphore!")?,
+                );
+            }
+            (
+                image_available_semaphores,
+                render_finished_semaphores,
+                in_flight_fences,
+            )
+        };
+
+        let render_pass = {
+            let color_attachment = Attachment {
+                format: Some(format),
+                samples: 1,
+                ops: AttachmentOps {
+                    load: AttachmentLoadOp::Clear,
+                    store: AttachmentStoreOp::Store,
+                },
+                stencil_ops: AttachmentOps::DONT_CARE,
+                layouts: Layout::Undefined..Layout::Present,
+            };
+            let subpass = SubpassDesc {
+                colors: &[(0, Layout::ColorAttachmentOptimal)],
+                depth_stencil: None,
+                inputs: &[],
+                resolves: &[],
+                preserves: &[],
+            };
+            unsafe {
+                device
+                    .create_render_pass(&[color_attachment], &[subpass], &[])
+                    .map_err(|_| "Couldn't create a render pass!")?
+            }
+        };
+
+        let image_views: Vec<_> = backbuffer
+            .into_iter()
+            .map(|image| unsafe {
+                device
+                    .create_image_view(
+                        &image,
+                        ViewKind::D2,
+                        format,
+                        Swizzle::NO,
+                        SubresourceRange {
+                            aspects: Aspects::COLOR,
+                            levels: 0..1,
+                            layers: 0..1,
+                        },
+                    )
+                    .map_err(|_| "Couldn't create the image_view for the image!")
+            })
+            .collect::<Result<Vec<_>, &str>>()?;
+
+        let framebuffers: Vec<<back::Backend as Backend>::Framebuffer> = {
+            image_views
+                .iter()
+                .map(|image_view| unsafe {
+                    device
+                        .create_framebuffer(
+                            &render_pass,
+                            vec![image_view],
+                            Extent {
+                                width: extent.width as u32,
+                                height: extent.height as u32,
+                                depth: 1,
+                            },
+                        )
+                        .map_err(|_| "Failed to create a framebuffer!")
+                })
+                .collect::<Result<Vec<_>, &str>>()?
+        };
+
+        let mut command_pool = unsafe {
+            device
+                .create_command_pool_typed(&queue_group, CommandPoolCreateFlags::RESET_INDIVIDUAL)
+                .map_err(|_| "Could not create the raw command pool!")?
+        };
+
+        let command_buffers: Vec<_> = framebuffers
+            .iter()
+            .map(|_| command_pool.acquire_command_buffer())
+            .collect();
+
+        Ok(Self {
+            _instance: ManuallyDrop::new(instance),
+            _surface: surface,
+            _adapter: adapter,
+            device: ManuallyDrop::new(device),
+            queue_group,
+            swapchain: ManuallyDrop::new(swapchain),
+            render_area: extent.to_extent().rect(),
+            render_pass: ManuallyDrop::new(render_pass),
+            image_views,
+            framebuffers,
+            command_pool: ManuallyDrop::new(command_pool),
+            command_buffers,
+            image_available_semaphores,
+            render_finished_semaphores,
+            in_flight_fences,
+            frames_in_flight,
+            current_frame: 0,
+        })
+    }
+
+    pub fn draw_clear_frame(
+        &mut self,
+        color: [f32; 4],
+    ) -> Result<Option<Suboptimal>, &'static str> {
+        // SETUP FOR THIS FRAME
+        // Advance the frame _before_ we start using the `?` operator
+        self.current_frame = (self.current_frame + 1) % self.frames_in_flight;
+
+        let mut image = false;
+        let (i_u32, i_usize) = unsafe {
+            let mut result = (0, 0);
+            while !image {
+                let check = self.swapchain.acquire_image(
+                    core::u64::MAX,
+                    Some(&self.image_available_semaphores[self.current_frame]),
+                    None,
+                );
+                match check {
+                    Ok(t) => {
+                        image = true;
+                        result = (t.0, t.0 as usize);
+                    }
+                    Err(_) => {
+                        self.recreate_swapchain();
+                    }
+                };
+            }
+            result
+        };
+
+        let image_available = &self.image_available_semaphores[self.current_frame];
+        let render_finished = &self.render_finished_semaphores[self.current_frame];
+
+        let flight_fence = &self.in_flight_fences[i_usize];
+        unsafe {
+            self.device
+                .wait_for_fence(flight_fence, core::u64::MAX)
+                .map_err(|_| "Failed to wait on the fence!")?;
+            self.device
+                .reset_fence(flight_fence)
+                .map_err(|_| "Couldn't reset the fence!")?;
+        }
+
+        unsafe {
+            let buffer = &mut self.command_buffers[i_usize];
+            let clear_values = [ClearValue::Color(ClearColor::Float(color))];
+            buffer.begin(false);
+            buffer.begin_render_pass_inline(
+                &self.render_pass,
+                &self.framebuffers[i_usize],
+                self.render_area,
+                clear_values.iter(),
+            );
+            buffer.finish();
+        }
+
+        let command_buffers = &self.command_buffers[i_usize..=i_usize];
+        let wait_semaphores: ArrayVec<[_; 1]> =
+            [(image_available, PipelineStage::COLOR_ATTACHMENT_OUTPUT)].into();
+        let signal_semaphores: ArrayVec<[_; 1]> = [render_finished].into();
+        let present_wait_semaphores: ArrayVec<[_; 1]> = [render_finished].into();
+        let submission = Submission {
+            command_buffers,
+            wait_semaphores,
+            signal_semaphores,
+        };
+        let the_command_queue = &mut self.queue_group.queues[0];
+        unsafe {
+            the_command_queue.submit(submission, Some(flight_fence));
+            self.swapchain
+                .present(the_command_queue, i_u32, present_wait_semaphores)
+                .map_err(|_| "Failed to present into the swapchain! {}")
+        }
+    }
+
+    fn create_swapchain(
+        surface: &mut <back::Backend as Backend>::Surface,
+        adapter: &Adapter<back::Backend>,
+        device: &<back::Backend as Backend>::Device,
+        old_swapchain: Option<<back::Backend as Backend>::Swapchain>
+    ) -> (
+        <back::Backend as Backend>::Swapchain,
+        Extent2D,
+        Vec<<back::Backend as Backend>::Image>,
+        Format,
+        usize,
+    ) {
+        let (caps, preferred_formats, present_modes) =
+            surface.compatibility(&adapter.physical_device);
+        info!("{:?}", caps);
+        info!("Preferred Formats: {:?}", preferred_formats);
+        info!("Present Modes: {:?}", present_modes);
+        info!("Composite Alphas: {:?}", caps.composite_alpha);
+        //
+        let present_mode = {
+            use gfx_hal::window::PresentMode::*;
+            match [Mailbox, Fifo, Relaxed, Immediate]
+                .iter()
+                .cloned()
+                .find(|pm| present_modes.contains(pm))
+                .ok_or("No PresentMode values specified!")
+            {
+                Ok(T) => T,
+                Err(E) => panic!(E),
+            }
+        };
+        let composite_alpha = {
+            use gfx_hal::window::CompositeAlpha;
+            match [
+                CompositeAlpha::OPAQUE,
+                CompositeAlpha::INHERIT,
+                CompositeAlpha::PREMULTIPLIED,
+                CompositeAlpha::POSTMULTIPLIED,
+            ]
+            .iter()
+            .cloned()
+            .find(|ca| caps.composite_alpha.contains(*ca))
+            .ok_or("No CompositeAlpha values specified!")
+            {
+                Ok(T) => T,
+                Err(E) => panic!(E),
+            }
+        };
+        let format = match preferred_formats {
+            None => Format::Rgba8Srgb,
+            Some(formats) => match formats
+                .iter()
+                .find(|format| format.base_format().1 == ChannelType::Srgb)
+                .cloned()
+            {
+                Some(srgb_format) => srgb_format,
+                None => match formats
+                    .get(0)
+                    .cloned()
+                    .ok_or("Preferred format list was empty!")
+                {
+                    Ok(T) => T,
+                    Err(E) => panic!(E),
+                },
+            },
+        };
+        let extent = caps.extents.end;
+        let image_count = if present_mode == PresentMode::Mailbox {
+            (caps.image_count.end - 1).min(caps.image_count.start.max(3))
+        } else {
+            (caps.image_count.end - 1).min(caps.image_count.start.max(2))
+        };
+        let image_layers = 1;
+        let image_usage = if caps.usage.contains(Usage::COLOR_ATTACHMENT) {
+            Usage::COLOR_ATTACHMENT
+        } else {
+            match Err("The Surface isn't capable of supporting color!") {
+                Ok(T) => T,
+                Err(E) => panic!(E),
+            }
+        };
+        let swapchain_config = SwapchainConfig {
+            present_mode,
+            composite_alpha,
+            format,
+            extent,
+            image_count,
+            image_layers,
+            image_usage,
+        };
+        info!("{:?}", swapchain_config);
+        //
+        let (swapchain, backbuffer) = unsafe {
+            device
+                .create_swapchain(surface, swapchain_config, old_swapchain)
+                .map_err(|_| "Failed to create the swapchain!")
+                .unwrap()
+        };
+        (swapchain, extent, backbuffer, format, image_count as usize)
+    }
+
+    pub fn recreate_swapchain(&mut self) {
+        use core::ptr::read;
+        self.device.wait_idle().unwrap();
+        unsafe {
+            self.command_pool.reset();
+            for framebuffer in self.framebuffers.drain(..) {
+                self.device.destroy_framebuffer(framebuffer);
+            }
+            for image_view in self.image_views.drain(..) {
+                self.device.destroy_image_view(image_view);
+            }
+            // LAST RESORT STYLE CODE, NOT TO BE IMITATED LIGHTLY
+            self.device
+                .destroy_swapchain(ManuallyDrop::into_inner(read(&self.swapchain)));
+        }
+        let old_swapchain = unsafe{ManuallyDrop::into_inner(read(&self.swapchain))};
+        let (swapchain, extent, backbuffer, format, frames_in_flight) =
+            Self::create_swapchain(&mut self._surface, &self._adapter, &self.device, Some(old_swapchain));
+
+        // Recreate and assing swapchain
+        self.swapchain = ManuallyDrop::new(swapchain);
+
+        self.image_views = backbuffer
+            .into_iter()
+            .map(|image| unsafe {
+                self.device
+                    .create_image_view(
+                        &image,
+                        ViewKind::D2,
+                        format,
+                        Swizzle::NO,
+                        SubresourceRange {
+                            aspects: Aspects::COLOR,
+                            levels: 0..1,
+                            layers: 0..1,
+                        },
+                    )
+                    .map_err(|_| "Couldn't create the image_view for the image!")
+            })
+            .collect::<Result<Vec<_>, &str>>()
+            .unwrap();
+
+        // Recreate framebuffers
+        self.framebuffers = {
+            self.image_views
+                .iter()
+                .map(|image_view| unsafe {
+                    self.device
+                        .create_framebuffer(
+                            &self.render_pass,
+                            vec![image_view],
+                            Extent {
+                                width: extent.width as u32,
+                                height: extent.height as u32,
+                                depth: 1,
+                            },
+                        )
+                        .map_err(|_| "Failed to create a framebuffer!")
+                })
+                .collect::<Result<Vec<_>, &str>>()
+                .unwrap()
+        };
+
+        self.render_area = extent.to_extent().rect();
+    }
+}
+
+impl core::ops::Drop for HalState {
+    /// We have to clean up "leaf" elements before "root" elements. Basically, we
+    /// clean up in reverse of the order that we created things.
+    fn drop(&mut self) {
+        self.device.wait_idle().unwrap();
+        unsafe {
+            for fence in self.in_flight_fences.drain(..) {
+                self.device.destroy_fence(fence)
+            }
+            for semaphore in self.render_finished_semaphores.drain(..) {
+                self.device.destroy_semaphore(semaphore)
+            }
+            for semaphore in self.image_available_semaphores.drain(..) {
+                self.device.destroy_semaphore(semaphore)
+            }
+            for framebuffer in self.framebuffers.drain(..) {
+                self.device.destroy_framebuffer(framebuffer);
+            }
+            for image_view in self.image_views.drain(..) {
+                self.device.destroy_image_view(image_view);
+            }
+            // LAST RESORT STYLE CODE, NOT TO BE IMITATED LIGHTLY
+            use core::ptr::read;
+            self.device.destroy_command_pool(
+                ManuallyDrop::into_inner(read(&self.command_pool)).into_raw(),
+            );
+            self.device
+                .destroy_render_pass(ManuallyDrop::into_inner(read(&self.render_pass)));
+            self.device
+                .destroy_swapchain(ManuallyDrop::into_inner(read(&self.swapchain)));
+            ManuallyDrop::drop(&mut self.device);
+            ManuallyDrop::drop(&mut self._instance);
+        }
+    }
+}
